9489793f4f12ae22323df403679888df2d57728f
Carmen-Sandeigo <97272236+Carmen-Sandeigo@users.noreply.github.com>
2022-03-07 18:41:20 -0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB3GsQwpgeY8Q_MDOoEC0RgAYsYcCOdnYQ",
+    authDomain: "kwitter-homework-app.firebaseapp.com",
+    projectId: "kwitter-homework-app",
+    storageBucket: "kwitter-homework-app.appspot.com",
+    messagingSenderId: "292998055412",
+    appId: "1:292998055412:web:b794a43c24651b6feca5a1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
